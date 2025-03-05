35c8f8121c44c5ce0252f69afd84c3422a74f63d
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-03-05 09:25:50 +0700

    Create about.vue

---
+++ frontend/src/pages/about.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container fluid class="pa-0">
+         <v-container fluid class="pa-0">
+
+        <v-img aspect-ratio="16/9" cover height="500"
+            src="https://images.unsplash.com/photo-1734106107054-b2f35096898c?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+            <div class="d-flex fill-height  align-end text-center pa-6">
+                <div class="text-h2 text-white font-weight-medium">
+                    About
+                </div>
+            </div>
+        </v-img>
+          </v-container>
+           <v-container >
+              <v-row>
+                <v-col cols="8"></v-col>
+                <v-col cols="4"></v-col>
+              </v-row>
+            </v-container>
+
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
