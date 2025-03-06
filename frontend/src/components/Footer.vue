8cd0f2eb7b9aeaaed1d0ae9f0282ce3fd80542f0
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-03-06 15:27:37 +0700

    Update Footer.vue

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,30 +1,27 @@
 <template>
-    <v-footer class="bg-blue-lighten-3 text-center d-flex flex-column">
-        <div>
-            <v-btn v-for="icon in icons" :key="icon" :icon="icon" class="mx-4" variant="text"></v-btn>
-        </div>
+  <v-footer class="bg-blue-lighten-3 text-center d-flex flex-column">
+    <div>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        class="mx-4"
+        variant="text"
+      ></v-btn>
+    </div>
 
-        <div class="pt-0">
-            "If you don't keep it, it'll disappear."
-        </div>
+    <div class="pt-0">"If you don't keep it, it'll disappear."</div>
 
-        <v-divider></v-divider>
+    <v-divider></v-divider>
 
-        <div>
-            {{ new Date().getFullYear() }} — <strong>2YOUR</strong>
-        </div>
-    </v-footer>
+    <div>{{ new Date().getFullYear() }} — <strong>2YOUR</strong></div>
+  </v-footer>
 </template>
 
 <script>
 export default {
-    data: () => ({
-        icons: [
-            'mdi-facebook',
-            'mdi-twitter',
-            'mdi-linkedin',
-            'mdi-instagram',
-        ],
-    }),
-}
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+};
 </script>
