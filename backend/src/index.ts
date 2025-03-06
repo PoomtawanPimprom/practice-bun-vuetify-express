1e1046ba58dd82b7f4cf491d3d2fb70bb8d9aa45
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-03-06 15:19:59 +0700

    use cors in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express, { type Request, type Response } from "express";
+import cors from "cors";  // เพิ่มการ import cors
 import UserRouter from "../src/router/user";
 import UserRepository from "../src/repository/user";
 import UserService from "../src/service/user";
@@ -14,23 +15,23 @@ const port = 8080;
 
 const startServer = async (): Promise<void> => {
 
-
   const userRepository = UserRepository.getInstance();
   const userService = UserService.getInstance(userRepository);
-  const userController = UserController.getInstance(userService)
+  const userController = UserController.getInstance(userService);
 
-  
   const roleRepository = RoleRepository.getInstance();
   const roleService = RoleService.getInstance(roleRepository);
-  const roleController = RoleController.getInstance(roleService)
+  const roleController = RoleController.getInstance(roleService);
 
+  const userRouter: UserRouter = UserRouter.getInstance(userController);
+  const roleRouter: RoleRouter = RoleRouter.getInstance(roleController);
 
-  const userRouter: UserRouter = UserRouter.getInstance(userController)
-  const roleRouter: RoleRouter = RoleRouter.getInstance(roleController)
+  app.use(cors());  // เพิ่มการใช้ cors ที่นี่
   app.use(express.json());
+  
   app
-    .use("/user",userRouter.getRouter(routerOptions))
-    .use("/role",roleRouter.getRouter(routerOptions))
+    .use("/user", userRouter.getRouter(routerOptions))
+    .use("/role", roleRouter.getRouter(routerOptions));
 
   app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
@@ -38,12 +39,11 @@ const startServer = async (): Promise<void> => {
 
   app.get("/api", (req: Request, res: Response) => {
     res.send("Hello World! from /api");
-  })
+  });
 
   app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
   });
-}
+};
 
-
-await startServer()
+await startServer();
