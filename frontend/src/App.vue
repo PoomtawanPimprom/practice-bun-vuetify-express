60c3213354dfcebc72ecbad788a84a5a44728fec
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-03-06 15:27:23 +0700

    update root component add footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,14 +4,17 @@
     <v-main class="pa-0" style="width: 100%;">
       <router-view />
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
+import Footer from './components/Footer.vue';
 import NavBar from './components/NavBar.vue';
 export default {
   components: {
     NavBar,
+    Footer,
   }
 }
 
