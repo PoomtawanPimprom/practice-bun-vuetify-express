c1ea5e77254d42ff1467a25c981bb5c149f4363d
Poomtawan Pimprom <poomtawan.work@gmail.com>
2025-03-06 15:30:47 +0700

    create shop page

---
+++ frontend/src/pages/shop/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-img
+    src="https://images.unsplash.com/photo-1544249804-78bcb97b5e65?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    aspect-ratio="16/9" 
+    cover 
+    height="500"
+    />
+     <v-container class="pa-8">
+        <p>"Because the people we love always deserve a bouquet."</p>
+        <v-row>
+          <v-col></v-col>
+        </v-row>
+      </v-container>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
